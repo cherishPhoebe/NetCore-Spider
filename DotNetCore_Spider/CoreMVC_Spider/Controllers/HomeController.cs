7e18acc89ed0042c11a8b8cdfcdc293542c8d138
tj_akd <384998447@qq.com>
2020-02-25 17:47:56 +0800

    添加NLog中间件

--- DotNetCore_Spider/CoreMVC_Spider/Controllers/HomeController.cs
+++ DotNetCore_Spider/CoreMVC_Spider/Controllers/HomeController.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CoreMVC_Spider.Models;
 using Microsoft.AspNetCore.Mvc;
-using CoreMVC_Spider.Models;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace CoreMVC_Spider.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            _logger.LogInformation("Index page says hello");
             return View();
         }
 
---
+++ DotNetCore_Spider/CoreMVC_Spider/Models/HouseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVC_Spider.Models
+{
+    public class HouseViewModel
+    {
+        public string Name { get; set; } 
+
+        public string Address { get; set; }
+
+
+
+    }
+}
