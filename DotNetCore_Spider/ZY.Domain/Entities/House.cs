1ea07bee4055cc0c5d0587ca7a5daafabd3b2f72
cherishPhoebe <cherish1224@outlook.com>
2020-03-16 21:31:02 +0800

    添加House实体

---
+++ DotNetCore_Spider/ZY.Domain/Entities/House.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZY.Domain.Entities
+{
+    public class House
+    {
+        public int Id { get; set; }
+    }
+}
